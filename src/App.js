fbeebd74c46f18cf1ae4b4fe9baac3763bbf7b70
Marine Hambaryan <marinehambaryan@Marines-MacBook-Pro.local>
2020-06-11 11:20:14 +0400

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import bgPentagon from "./images/bg-pentagon.svg";
+import bgTriangle from "./images/bg-triangle.svg";
+import iconClose from "./images/icon-close.svg";
+import iconLizard from "./images/icon-lizard.svg";
+import iconPaper from "./images/icon-paper.svg";
+import iconRock from "./images/icon-rock.svg";
+import iconScissors from "./images/icon-scissors.svg";
+import iconSpock from "./images/icon-spock.svg";
+import imageRulesBonus from "./images/image-rules-bonus.svg";
+import imageRules from "./images/image-rules.svg";
+import logoBonus from "./images/logo-bonus.svg";
+import logo from "./images/logo.svg";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={bgPentagon} />
+      <img src={bgTriangle} />
+      <img src={iconClose} />
+      <img src={iconLizard} />
+      <img src={iconPaper} />
+      <img src={iconRock} />
+      <img src={iconScissors} />
+      <img src={iconSpock} />
+      <img src={imageRulesBonus} />
+      <img src={imageRules} />
+      <img src={logoBonus} />
+      <img src={logo} />
     </div>
   );
 }
